3c1b19af954970cee85b7bbe45ab7f73b68c8017
Vinay Duggineni <161632323+vinay-duggi@users.noreply.github.com>
2026-05-05 16:37:24 +0530

    Authentication-Registration-Login with JWT

--- models.py
+++ models.py
@@ -15,6 +15,7 @@ class User(Base):
     id: Mapped[int] = mapped_column(Integer, primary_key=True, index=True)
     username: Mapped[str] = mapped_column(String(50), unique=True, nullable=False)
     email: Mapped[str] = mapped_column(String(120), unique=True, nullable=False)
+    password_hash: Mapped[str] = mapped_column(String(200), nullable=False)
 
     posts: Mapped[list[Post]] = relationship(back_populates="author", cascade="all, delete-orphan",)
 
--- schemas.py
+++ schemas.py
@@ -9,18 +9,28 @@ class UserBase(BaseModel):
 
 
 class UserCreate(UserBase):
-    pass
+    password: str = Field(min_length=8)
 
 
-class UserResponse(UserBase):
+class UserPublic(BaseModel):
     model_config = ConfigDict(from_attributes=True)
 
     id: int
+    username: str
+
+
+class UserPrivate(UserPublic):
+    email: EmailStr
 
 
 class UserUpdate(BaseModel):
     username: str | None = Field(default=None, min_length=1, max_length=50)
     email: EmailStr | None = Field(default=None, max_length=120)
+
+
+class Token(BaseModel):
+    access_token: str
+    token_type: str
 
 
 class PostBase(BaseModel):
@@ -43,4 +53,4 @@ class PostResponse(PostBase):
     id: int
     user_id: int
     date_posted: datetime
-    author: UserResponse
+    author: UserPublic
